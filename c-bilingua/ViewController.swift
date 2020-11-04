78ea27f3c00e6e6cbe111a247c4745564bf714c7
carooobregon <caroobregon@gmail.com>
2020-11-03 20:41:58 -0700

    first commit

---
+++ c-bilingua/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  c-bilingua
+//
+//  Created by Caro Obregon on 03/11/20.
+//  Copyright © 2020 cbmt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
