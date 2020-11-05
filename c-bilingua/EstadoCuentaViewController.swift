2ffbe693def14121a55c40a7acd90fe0552acacf
user182115 <user182115@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-11-05 14:03:11 -0500

    Estados de cuenta

---
+++ c-bilingua/EstadoCuentaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EstadoCuentaViewController.swift
+//  c-bilingua
+//
+//  Created by user182115 on 11/5/20.
+//  Copyright © 2020 cbmt. All rights reserved.
+//
+
+import UIKit
+
+class EstadoCuentaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
