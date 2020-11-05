abe20a0481e876feecc0b7f3244f19abb1cb4750
gabrielacorona <gabriela.corona03@gmail.com>
2020-11-05 13:10:21 -0600

    pulled from master

---
+++ c-bilingua/Views/AvisoCompletoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AvisoCompletoViewController.swift
+//  c-bilingua
+//
+//  Created by Caro Obregon on 05/11/20.
+//  Copyright © 2020 cbmt. All rights reserved.
+//
+
+import UIKit
+
+class AvisoCompletoViewController: UIViewController {
+    var currAviso : Aviso?
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var body: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titulo.text = currAviso?.titulo
+        body.text = currAviso?.body
+        // Do any additional setup after loading the view.
+    }
+
+    // todo: implement scroll view for large texts
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
