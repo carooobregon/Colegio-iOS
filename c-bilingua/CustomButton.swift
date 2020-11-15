34f7c497b9d657efcc1ba7f33f2e2fe65f5fde26
carooobregon <caroobregon@gmail.com>
2020-11-14 22:23:29 -0700

    trying to fix button layouts

---
+++ c-bilingua/CustomButton.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+final class CustomButton: UIButton {
+
+    public var shadowLayer: CAShapeLayer!
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        if shadowLayer == nil {
+            shadowLayer = CAShapeLayer()
+            shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 12).cgPath
+            shadowLayer.fillColor = UIColor.systemPink.cgColor
+
+            shadowLayer.shadowColor = UIColor.darkGray.cgColor
+            shadowLayer.shadowPath = shadowLayer.path
+            shadowLayer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+            shadowLayer.shadowOpacity = 0.8
+            shadowLayer.shadowRadius = 2
+
+            layer.insertSublayer(shadowLayer, at: 0)
+            //layer.insertSublayer(shadowLayer, below: nil) // also works
+        }
+    }
+    
+    
+    func changeColor(){
+        shadowLayer.fillColor = UIColor.systemYellow.cgColor
+
+    }
+
+}
--- c-bilingua/Views/MenuViewController.swift
+++ c-bilingua/Views/MenuViewController.swift
@@ -10,10 +10,46 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
+    @IBOutlet weak var btnCalif: CustomButton!
+    @IBOutlet weak var btnAvisos: CustomButton!
+    @IBOutlet weak var btnCalendario: CustomButton!
+    @IBOutlet weak var btnEstado: CustomButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        assignButtonColors()
         // Do any additional setup after loading the view.
+        
     }
+    
+    func assignButtonColors(){
+        btnCalif.shadowLayer.fillColor = UIColor.lightGray.cgColor
+
+        btnAvisos.shadowLayer.fillColor = UIColor.lightGray.cgColor
+
+    }
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+           var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+           if (cString.hasPrefix("#")) {
+               cString.remove(at: cString.startIndex)
+           }
+
+           if ((cString.count) != 6) {
+               return UIColor.gray
+           }
+
+           var rgbValue:UInt64 = 0
+           Scanner(string: cString).scanHexInt64(&rgbValue)
+
+           return UIColor(
+               red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+               green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+               blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+               alpha: CGFloat(1.0)
+           )
+       }
 //    override func viewDidDisappear(_ animated: Bool) {
 //        self.navigationController!.isNavigationBarHidden = true;
 //    }
