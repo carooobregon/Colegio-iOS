503b5f2dbb788073e527524611a080770ae1b5b6
carooobregon <caroobregon@gmail.com>
2020-11-14 19:14:52 -0700

    added password vc

---
+++ c-bilingua/Views/ForgotPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ForgotPasswordViewController.swift
+//  c-bilingua
+//
+//  Created by Caro Obregon on 14/11/20.
+//  Copyright © 2020 cbmt. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
