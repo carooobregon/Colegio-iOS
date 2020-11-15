26779b0ffb9d73b8e8c42539eaa7bff83cc766ba
carooobregon <caroobregon@gmail.com>
2020-11-14 19:47:45 -0700

    added sign up auth w firebase

--- c-bilingua/Views/ViewController.swift
+++ c-bilingua/Views/ViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
 
+    @IBOutlet weak var txtUsuario: UITextField!
+    @IBOutlet weak var txtContrasena: UITextField!
     
     
     
@@ -38,6 +41,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func registerAction(_ sender: Any) {
+        if txtUsuario.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+        
+        } else {
+            Auth.auth().createUser(withEmail: txtUsuario.text!, password: txtContrasena.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                    //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
 
+    }
+    
 }
 
